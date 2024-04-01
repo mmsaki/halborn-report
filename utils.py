dd0ecbbda5200ebee844f9a40f85d7c470204032
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2024-04-01 18:59:45 -0500

    refactor necessary utility functions

--- utils.py
+++ utils.py
@@ -1,126 +1,65 @@
 import re
+import json
 
-# converts cwe list to one template string
-# in  -> [{'cwe_id': 'CWE-682', 'name': 'Incorrect Calculation'}, {'cwe_id': 'CWE-709', 'name': 'Incorrect Calculation'}]
-# out -> '{url_cwe}682.html[CWE-682], {url_cwe}709.html[CWE-709]'
-def get_cwes(cwes):
-  ids = [i['cwe_id'] for i in cwes]
-  links = ['{url-cwe}' + f'{cwes.split("-")[1]}.html[{cwes}]' for cwes in ids]
-  return ", ".join(links)
+def get_classified_vunerabilities(raw_data):
+  """Takes raw json data from dependabot and classifies them according to serverity"""
+  
+  medium = [] 
+  high = []
+  low = []
+  critical = []
+  for vul in raw_data:
+    if vul["security_advisory"]["severity"].lower() == "medium":
+      medium.append(vul)
+    elif vul["security_advisory"]["severity"].lower() == "low":
+      low.append(vul)
+    elif vul["security_advisory"]["severity"].lower() == "high":
+      high.append(vul)
+    elif vul["security_advisory"]["severity"].lower() == "critical":
+      critical.append(vul)
+    else:
+      low.append(vul)
+  return (critical, high, medium, low)
 
+def save_issues(data: list):
+  """Creates separate json files accodrding to severity"""
 
-# Finds the line number where dependecy was used in a Cargo.Lock file
+  (criticals, highs, mediums, lows) = get_classified_vunerabilities(data)
+  MEDIUMS_PATH = "./mediums.json"
+  LOWS_PATH = "./lows.json"
+  HIGHS_PATH = "./highs.json"
+  CRITICALS_PATH = "./criticals.json"
+
+  with open(MEDIUMS_PATH, 'w') as file:
+    file.write(json.dumps(mediums))
+
+  with open(LOWS_PATH, 'w') as file:
+    file.write(json.dumps(lows))
+
+  with open(HIGHS_PATH, 'w') as file:
+    file.write(json.dumps(highs))
+
+  with open(CRITICALS_PATH, 'w') as file:
+    file.write(json.dumps(criticals))
+
+  print("done")
+
 def find_line(dependency, filename):
+  """Finds the line number where dependecy was used in filepath"""
+
   lookup = dependency
   with open(filename) as myFile:
-    for num, line in enumerate(myFile, 1):
+    for num, line in enumerate(myFile, 1): # first line is not zero indexed
       if lookup in line:
         return num
 
-# gets the serverity of issue and assigns a symbol
-def get_symbol(severity):
-  sym = ""
-  if severity.lower() == "medium":
-    sym = "M-"
-  elif severity.lower() == "critical":
-    sym = "C-"
-  elif severity.lower() == "low":
-    sym = "L-"
-  else:
-    sym = ""
-  return sym
-
-# converts a markdown links to asciidoc links
-# in  -> [My Website](https://msaki.io)
-# out -> https://msaki.io[My Website]
-def convert_md_link(string):
-  pattern = '(\[)(.+?)(\])(\()(.+?)(\))'
-  p = re.compile(pattern)
-  v = p.findall(string)
-  return v[0][4] + "".join(v[0][0:3])
-
-# Converts .md syntax to .asccidoc syntax
-# in  -> "### My Links\n\n* [Web](https://msaki.io)\n* [Github](https://github.com/mmsaki)\n* Thank you"
-# out -> "### My Links\n\n* https://msaki.io[Web]\n* https://github.com/mmsaki[Github]\n* Thank you"
-def convert_desc(desc):
-  pattern = '(\[)(.+?)(\])(\()(.+?)(\))'
-  p = re.compile(pattern)
-  v = p.finditer(desc)
-  count = 0
-  for m in v:
-    start = m.start()
-    end = m.end()
-    group = m.group(0)
-    desc = desc[0:start - count] + convert_md_link(group) + desc[end - count:]
-    count += 2
-  desc = desc.replace("#### ", "=== ")
-  desc = desc.replace("### ", "=== ")
-  desc = desc.replace("## ", "=== ")
-  desc = desc.replace("# ", "=== ")
-  return desc
-
-# get body of Impact description
-def get_impact(string):
-  pattern = '(?<==== Impact\n\n)((\s*)(.+)(\s*))+?(?====)'
-  g = re.search(pattern, string)
-  try:
-    start = g.start()
-    end = g.end()
-  except:
-    return ""
-  return string[start:end].rstrip()
-
-# get body of Patches description
-def get_patches(string):
-  pattern = '(?<==== Patches\n\n)((\s*)(.+)(\s*))+?(?====)'
-  g = re.search(pattern, string)
-  try:
-    start = g.start()
-    end = g.end()
-  except:
-    return ""
-  return string[start:end].rstrip()
-
-# get body of Workarounds description
-def get_workarounds(string):
-  pattern = '(?<==== Workarounds\n\n)((\s*)(.+)(\s*))+?(?====)'
-  g = re.search(pattern, string)
-  try:
-    start = g.start()
-    end = g.end()
-  except:
-    return ""
-  return string[start:end].rstrip()
-
-# get body of References description
-def get_references(string):
-  pattern = '(?<==== References\n\n)((\s*)(.+)(\s*))+?(?====)'
-  g = re.search(pattern, string)
-  try:
-    start = g.start()
-    end = g.end()
-  except:
-    return ""
-  return string[start:end].rstrip()
-
-# get body of For more information description
-def get_for_more_information(string):
-  pattern = '(?<==== For more information\n\n)((\s*)(.+)(\s*))+?'
-  g = re.search(pattern, string)
-  try:
-    start = g.start()
-    # assumes this is the last section in descrition so we use len(string) as last index
-    end = len(string)
-  except:
-    return ""
-  return string[start:end].rstrip()
-
-
-
-# gets cvss3.1 level impact and assigns equivalent long string name 
-# in  -> "CVSS:3.1/AV:L/AC:H/PR:H/UI:R/S:U/C:L/I:L/A:L"
-# out -> ('Local', 'High', 'High', 'High', 'Required', 'Unchange', 'Low', 'Low', 'Low')
 def get_cvss(cvss):
+  """
+  Gets cvss3.1 level impact and assigns equivalent long string name 
+  in  -> "CVSS:3.1/AV:L/AC:H/PR:H/UI:R/S:U/C:L/I:L/A:L"
+  out -> ('Local', 'High', 'High', 'High', 'Required', 'Unchange', 'Low', 'Low', 'Low')
+  """
+
   # initialize cvss variables
   AV, AC, PR, PR, UI, S, C, I, A = "","","","","","","","",""
   try:
@@ -128,10 +67,10 @@ def get_cvss(cvss):
     AC = cvss[re.search('/AC:', cvss).span()[1]]
     PR = cvss[re.search('/PR:', cvss).span()[1]]
     UI = cvss[re.search('/UI:', cvss).span()[1]]
-    S = cvss[re.search('/S:', cvss).span()[1]]
-    C = cvss[re.search('/C:', cvss).span()[1]]
-    I = cvss[re.search('/I:', cvss).span()[1]]
-    A = cvss[re.search('/A:', cvss).span()[1]]
+    S  = cvss[re.search('/S:', cvss).span()[1]]
+    C  = cvss[re.search('/C:', cvss).span()[1]]
+    I  = cvss[re.search('/I:', cvss).span()[1]]
+    A  = cvss[re.search('/A:', cvss).span()[1]]
   except:
     pass
 
