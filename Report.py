10228b120b5ef80fe781aa0e7c156f735c8afbed
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2024-04-01 14:14:29 -0500

    add tags

--- Report.py
+++ Report.py
@@ -26,31 +26,41 @@ class Report():
       raise StopIteration
 
 class ReportItem():
-  def __init__(self, issue, severity, number):
+  # Constants
+  URL_CWE  = "{url-cwe}"
+  URL_NIST = "{url-nist}"
+  URL_GHSA = "{url-ghsa}"
+  URL_REPO = "{url-repo}"
+  URL_BLOB = "{url-blob}"
+  FILE_DIR = "{ctf-dir}"
+
+  def __init__(self, issue: Issue, severity: str, number: int):
     self.issue = issue
     self.type = severity.upper()[0]
     self.number = number
     self.title = self.type + "-" + str(self.number) + " " + issue.summary
     self.desc = self.conver_to_asciidoc(issue.description)
+    self.cwes = self.get_cwes(issue.cwes)
+    self.tags = f"Tags: `{issue.scope}`{f', Weakness: {self.cwes}' if self.cwes else ''}{f', CVE ID: {self.URL_NIST}{issue.cve_id}[{issue.cve_id}]' if issue.cve_id else ''}{f', GHSA ID: {self.URL_GHSA}{issue.ghsa_id}[{issue.ghsa_id}]' if issue.ghsa_id else ''}"
 
   def __str__(self) -> str:
     return f"""## tag::{self.number}[]
 == {self.title}
+{self.tags}
 
 {self.desc}
-
 
 ## end::{self.number}[]
 """
 
   def conver_to_asciidoc(self, string) -> str:
     """
-    Converts .md syntax to .asccidoc syntax
+    Converts markdown syntax to asccidoc syntax
 
     in:  \n* [Blog](https://msaki.io)\n* [Github](https://github.com/mmsaki)\n* Thank you\n
     out: \n* https://msaki.io[Blog]\n* https://github.com/mmsaki[Github]\n* Thank you
     """
-    pattern = '(\[)(.+?)(\])(\()(.+?)(\))'
+    pattern = r'(\[)(.+?)(\])(\()(.+?)(\))'
     grep = re.compile(pattern)
     try:
       found = grep.finditer(string)
@@ -76,7 +86,13 @@ class ReportItem():
     in:  [My Website](https://msaki.io)
     out: https://msaki.io[My Website]
     """
-    pattern = '(\[)(.+?)(\])(\()(.+?)(\))'
+    pattern = r'(\[)(.+?)(\])(\()(.+?)(\))'
     grep = re.compile(pattern)
     found = grep.findall(string)
     return found[0][4] + "".join(found[0][0:3])
+  
+  def get_cwes(self, cwes):
+    """Converts all cwes to asciidoc format"""
+    ids = [i['cwe_id'] for i in cwes]
+    links = [self.URL_CWE + f'{cwes.split("-")[1]}.html[{cwes}]' for cwes in ids]
+    return ", ".join(links)
