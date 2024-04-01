e9e31f31bcadc462b606773828a62aad463e0462
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2024-04-01 09:22:14 -0500

    add convert to asciidoc method

--- Report.py
+++ Report.py
@@ -30,23 +30,41 @@ class ReportItem():
     self.issue = issue
     self.type = severity.upper()[0]
     self.number = number
-    self.title = self.type + "-" + str(self.number) + " " + self.issue.summary
-    self.get_desc()
+    self.title = self.type + "-" + str(self.number) + " " + issue.summary
+    self.desc = self.conver_to_asciidoc(issue.description)
 
-  def get_desc(self):
-    if self.issue.is_markdown:
-      pattern = '(?<=# Impact\n\n)((\\s*)(.+)(\\s*))+?(?=)'
-      grep = re.search(pattern, self.issue.description)
-      try:
-        start = grep.start()
-        end = grep.end()
-        self.desc = self.issue.description[start:end].rstrip()
-        self.has_impact = True
-      except:
-        # is markdown but no impact title
-        self.has_impact = False
-        self.desc = self.issue.description  
-    else:
-      self.has_impact = False
-      self.desc = self.issue.description
-
+  def conver_to_asciidoc(self, string):
+    """
+    Converts .md syntax to .asccidoc syntax
+      in:  "### My Links\n\n* [Blog](https://msaki.io)\n* [Github](https://github.com/mmsaki)\n* Thank you"
+      out: "### My Links\n\n* https://msaki.io[Blog]\n* https://github.com/mmsaki[Github]\n* Thank you"
+    """
+    pattern = '(\[)(.+?)(\])(\()(.+?)(\))'
+    grep = re.compile(pattern)
+    try:
+      found = grep.finditer(string)
+      count = 0
+      for item in found:
+        start = item.start()
+        end = item.end()
+        group = item.group(0)
+        string = string[0:start - count] + self.convert_md_link(group) + string[end - count:]
+        count += 2
+    except:
+      print("Reached Except")
+    string = string.replace("####", "===")
+    string = string.replace("###", "===")
+    string = string.replace("##", "===")
+    string = string.replace("#", "===")
+    return string
+    
+  def convert_md_link(self, string):
+    """
+    Converts a markdown links to asciidoc links
+      in:  [My Website](https://msaki.io)
+      out: https://msaki.io[My Website]
+    """
+    pattern = '(\[)(.+?)(\])(\()(.+?)(\))'
+    grep = re.compile(pattern)
+    found = grep.findall(string)
+    return found[0][4] + "".join(found[0][0:3])
