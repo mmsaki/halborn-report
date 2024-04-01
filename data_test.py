cc1bbdd3c9ca647ce82c5020c3a770a147da7847
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2024-03-31 22:02:05 -0500

    add test for non null values

--- data_test.py
+++ data_test.py
@@ -6,56 +6,56 @@ from DependabotData import DependabotData
 # open dependabot results.json and store results in `data` variable
 
 @pytest.fixture
-def data():
-  path = './mediums.json'
+def test_data():
+  path = './results.json'
   with open(path, 'r') as file:
     data = json.load(file)
 
-  return data
+  yield DependabotData(data)
 
 class TestDependabotData:
-  def test_one(self, data):
-    self.data = DependabotData(data)
-    assert all(issue for issue in self.data)
-
-  def test_two(self, data):
-    pass
-
+  def test_all_issues_present(self, test_data):
+    self.issues = test_data
+    assert all(issue for issue in self.issues)
   
-# res = DependabotData(data)
-
-# issue = res[9]
-# print(f"number: {issue.number}")
-# print(f"state: {issue.state}")
-# print(f"ecosystem: {issue.ecosystem}")
-# print(f"package: {issue.package}")
-# print(f"name: {issue.name}")
-# print(f"path: {issue.path}")
-# print(f"scope: {issue.scope}")
-# # print(f"advisory: {issue.advisory}")
-# print(f"ghsa_id: {issue.ghsa_id}")
-# print(f"cve_id: {issue.cve_id}")
-# print(f"summary: {issue.summary}")
-# # print(f"description: {issue.description}")
-# print(f"severity: {issue.severity}")
-# print(f"identifiers: {issue.all_ids}")
-# # print(f"references: {issue.references}")
-# print(f"published_at: {issue.published_at}")
-# print(f"updated_at: {issue.updated_at}")
-# print(f"withdrawn_at: {issue.withdrawn_at}")
-# print(f"vulnerabilities: {issue.vulnerabilities}")
-# print(f"cvss: {issue.cvss}")
-# print(f"score: {issue.score}")
-# print(f"cwes: {issue.all_cwes}")
-# print(f"vulnerability: {issue.vulnerability}")
-# print(f"vulnerabile_package: {issue.vulnerability}")
-# print(f"vulnerability_serverity: {issue.vulnerability_serverity}")
-# print(f"vulnerability_range: {issue.vulnerability_range}")
-# print(f"vulnerability_patch: {issue.vulnerability_patch}")
-# print(f"url: {issue.url}")
-# print(f"html_url: {issue.html_url}")
-# print(f"created_at: {issue.created_at}")
-# print(f"updated_at: {issue.updated_at}")
-# print(f"dismissed_at: {issue.dismissed_at}")
-# print(f"dismissed_by: {issue.dismissed_by}")
-# print(f"dismissed_reason: {issue.dismissed_reason}")
+  def test_all_issues_with_values(self, test_data):
+    self.issues = test_data
+    assert all(
+      all([
+        issue.number,
+        issue.state,
+        issue.ecosystem,
+        issue.package,
+        issue.name,
+        issue.path,
+        issue.scope,
+        issue.advisory,
+        issue.ghsa_id,
+        # issue.cve_id,                 (not all issues have)
+        issue.summary,
+        issue.description,
+        issue.severity,
+        issue.identifiers,
+        issue.references,
+        issue.published_at,
+        issue.updated_at,
+        # issue.withdrawn_at,           (not all issues have)
+        issue.vulnerabilities,
+        # issue.cvss,                   (not all issues have)
+        # issue.score,                  (not all issues have)
+        # issue.cwes,                   (not all issues have)
+        issue.vulnerability,
+        # issue.vulnerabile_package,    (not all issues have)
+        issue.vulnerability_serverity,
+        issue.vulnerability_range,
+        # issue.vulnerability_patch,    (not all issues have)
+        issue.url,
+        issue.html_url,
+        issue.created_at,
+        issue.updated_at,
+        # issue.dismissed_at,           (not all issues have)
+        # issue.dismissed_by,           (not all issues have)
+        # issue.dismissed_reason,       (not all issues have)
+      ])
+      for issue in self.issues
+      )
