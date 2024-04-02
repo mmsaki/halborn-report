65886e711ac27a95b8a5bff511455d36e0d293c3
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2024-04-01 19:02:05 -0500

    add tests to check for permalink and tags

--- tests/data_sematics_test.py
+++ tests/data_sematics_test.py
@@ -8,3 +8,11 @@ class TestReport:
   def test_has_desc(self, all_reports):
     self.data = all_reports
     assert all(report.desc for report in self.data)
+  
+  def test_has_tags(self, all_reports):
+    self.data = all_reports
+    assert all(report.tags for report in self.data)
+  
+  def test_has_permalink(self, all_reports):
+    self.data = all_reports
+    assert all(report.permalink for report in self.data)
